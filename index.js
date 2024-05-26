9aa5c6671f876e1d9625d47f15bcf7dbebac38bd
HighonHTML <125417055+HighonHTML@users.noreply.github.com>
2024-05-26 17:44:53 +0530

    fill the templates and parse variables from URL

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require('node:url');
 
 // const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textInput);
@@ -20,14 +21,57 @@ const http = require("http");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+function replaceTemplate(temp, product) {
+
+  // use a regualr expression to replace every instance and not just the first one
+  let object = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  object = object.replace(/{%ID%}/g, product.id);
+  object = object.replace(/{%IMAGE%}/g, product.image);
+  object = object.replace(/{%FROM%}/g, product.from);
+  object = object.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  object = object.replace(/{%QUANTITY%}/g, product.quantity);
+  object = object.replace(/{%PRICE%}/g, product.price);
+  if (!product.organic) {
+    object = object.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  object = object.replace(/{%DESCRIPTION%}/g, product.description);
+  return object;
+}
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("welcome to the overview");
-  } else if (pathName === "/product") {
-    res.end("welcome to the product");
-  } else if (pathName === "/api") {
+  const {pathname, query} = url.parse(req.url, true)
+ 
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const cardHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+    res.end(output);
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObject[query.id]
+    const output = replaceTemplate(tempProduct, product)
+    console.log(output)
+    res.end(output);
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
